58fe6276d2a70581e4c74cb01c9ad709edfd6e17
fadillahmuhammad <fadillahmuhammad1102@gmail.com>
2023-09-21 16:12:31 +0700

    first commit

---
+++ app/src/main/java/com/dicoding/myflexiblefragment/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.dicoding.myflexiblefragment
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.commit
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val mFragmentManager = supportFragmentManager
+        val mHomeFragment = HomeFragment()
+        val fragment = mFragmentManager.findFragmentByTag(HomeFragment::class.java.simpleName)
+
+        if (fragment !is HomeFragment) {
+            Log.d("MyFlexibleFragment", "Fragment Name :" + HomeFragment::class.java.simpleName)
+            mFragmentManager.commit {
+                add(R.id.frame_container, mHomeFragment, HomeFragment::class.java.simpleName)
+            }
+        }
+    }
+}
